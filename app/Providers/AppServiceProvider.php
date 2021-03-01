ab04f9b6415ea0fa1f58f23ead349cac37529149
Yee Choong <yeechoong97@gmail.com>
2021-03-01 18:03:14 +0800

    Test SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-// use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
