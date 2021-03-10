74a26df70b73629e8b8d13cbf9d5ef52504bfd62
Yee Choong <yeechoong97@gmail.com>
2021-03-10 14:20:52 +0800

    Update User Factory

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -9,8 +9,8 @@ use Faker\Generator as Faker;
 $factory->define(Account::class, function (Faker $faker) {
     return [
         'user_id' => 'EST'.$faker->randomDigit,
-        'balance' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = NULL),
-        'margin'=> $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = NULL),
+        'balance' => $faker->randomFloat($nbMaxDecimals = 2, $min = 50, $max = NULL),
+        'margin'=> $faker->randomFloat($nbMaxDecimals = 2, $min = 50, $max = NULL),
         'margin_used'=> $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100.00),
         'leverage' => $faker->randomElement(['50 : 1','40 : 1','30 : 1','20 : 1','10 : 1'])
     ];
