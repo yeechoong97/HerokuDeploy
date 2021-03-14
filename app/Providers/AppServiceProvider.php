8b84bb3774ced92af13163bb3a2ace428e434ae5
Yee Choong <yeechoong97@gmail.com>
2021-03-14 17:22:26 +0800

    Removed SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
         Schema::defaultStringLength(191);
     }
 }
