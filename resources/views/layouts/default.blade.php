ffae817ddf530bf24c8c0396b6eb911194718a4c
Yee Choong <yeechoong97@gmail.com>
2021-03-19 18:41:16 +0800

    Reduce Render Speed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -42,11 +42,6 @@
 </body>
     <script type="text/javascript" src="{{ URL::asset('js/intro.js') }}"></script> 
     <script type="text/javascript" src="{{ URL::asset('js/calculator.js') }}"></script>  
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js" integrity="sha384-0gXGTz/tBVgjh6A4quL+s/hguKQl89dE8KvkugzXlYULoh//f3XfOhU0oENHkIAJ" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-ui.min.js" integrity="sha384-gwyymOUwBjZeBlxG4h2aWpibl0PpwvRE4xfGd6z+cPlwtMv9S3ls6fESCm/83FIY" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-stock.min.js" integrity="sha384-EipjguUjfJz8ICBl7RXzdLMUzAmUiPakm52M0jwRgD3HcZXp02N2wI5+U0+7gB5P" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-data-adapter.min.js" integrity="sha384-MjvdhJD1JvNJkJYeQFjUXT7xSAuGw4uP8O6+C2i/e2nCXZqnfFOC9WwO0EZ7ujNj" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/8.9.0/js/anychart-annotations.min.js" integrity="sha384-tPDIlLMU5AutTu0NWf+PHo4ADSGWE/GL1IxDDSxsdl+lJtoI8dBM/bi5GpqIBRIx" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.all.min.js" integrity="sha384-cBSLl8QUzUYEENH3ekFxzQ+mNnUW0Rv2b5PUUV6TtsjlwX6ywxH3Im6qtXhbagVc" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
