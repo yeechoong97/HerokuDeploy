b91aa4a322619694c128af1cecd5870300dc3326
Yee Choong <yeechoong97@gmail.com>
2021-03-19 18:39:57 +0800

    Reduce Render Speed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -26,11 +26,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/3.2.1/introjs.min.css" integrity="sha512-631ugrjzlQYCOP9P8BOLEMFspr5ooQwY3rgt8SMUa+QqtVMbY/tniEUOcABHDGjK50VExB4CNc61g5oopGqCEw==" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha384-KcyRSlC9FQog/lJsT+QA8AUIFBgnwKM7bxm7/YaX+NTr4D00npYawrX0h+oXI3a2" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha384-JPbtLYL10d/Z1crlc6GGGGM3PavCzzoUJ1UxH0bXHOfguWHQ6XAWrIzW+MBGGXe5" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js" integrity="sha384-0gXGTz/tBVgjh6A4quL+s/hguKQl89dE8KvkugzXlYULoh//f3XfOhU0oENHkIAJ" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-ui.min.js" integrity="sha384-gwyymOUwBjZeBlxG4h2aWpibl0PpwvRE4xfGd6z+cPlwtMv9S3ls6fESCm/83FIY" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-stock.min.js" integrity="sha384-EipjguUjfJz8ICBl7RXzdLMUzAmUiPakm52M0jwRgD3HcZXp02N2wI5+U0+7gB5P" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-data-adapter.min.js" integrity="sha384-MjvdhJD1JvNJkJYeQFjUXT7xSAuGw4uP8O6+C2i/e2nCXZqnfFOC9WwO0EZ7ujNj" crossorigin="anonymous"></script>
-    <script src="https://cdn.anychart.com/releases/8.9.0/js/anychart-annotations.min.js" integrity="sha384-tPDIlLMU5AutTu0NWf+PHo4ADSGWE/GL1IxDDSxsdl+lJtoI8dBM/bi5GpqIBRIx" crossorigin="anonymous"></script>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 
@@ -47,6 +42,11 @@
 </body>
     <script type="text/javascript" src="{{ URL::asset('js/intro.js') }}"></script> 
     <script type="text/javascript" src="{{ URL::asset('js/calculator.js') }}"></script>  
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js" integrity="sha384-0gXGTz/tBVgjh6A4quL+s/hguKQl89dE8KvkugzXlYULoh//f3XfOhU0oENHkIAJ" crossorigin="anonymous"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-ui.min.js" integrity="sha384-gwyymOUwBjZeBlxG4h2aWpibl0PpwvRE4xfGd6z+cPlwtMv9S3ls6fESCm/83FIY" crossorigin="anonymous"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-stock.min.js" integrity="sha384-EipjguUjfJz8ICBl7RXzdLMUzAmUiPakm52M0jwRgD3HcZXp02N2wI5+U0+7gB5P" crossorigin="anonymous"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-data-adapter.min.js" integrity="sha384-MjvdhJD1JvNJkJYeQFjUXT7xSAuGw4uP8O6+C2i/e2nCXZqnfFOC9WwO0EZ7ujNj" crossorigin="anonymous"></script>
+    <script src="https://cdn.anychart.com/releases/8.9.0/js/anychart-annotations.min.js" integrity="sha384-tPDIlLMU5AutTu0NWf+PHo4ADSGWE/GL1IxDDSxsdl+lJtoI8dBM/bi5GpqIBRIx" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.all.min.js" integrity="sha384-cBSLl8QUzUYEENH3ekFxzQ+mNnUW0Rv2b5PUUV6TtsjlwX6ywxH3Im6qtXhbagVc" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
