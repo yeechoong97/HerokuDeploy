24437c4554abdc82cd2877aa3531a917ab1cfe7c
Yee Choong <yeechoong97@gmail.com>
2021-03-19 21:52:22 +0800

    Improve SEO

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,8 @@
 <head>
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<meta charset="UTF-8">
-    <meta name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ES Forex Trading System">
 	<title>ES FOREX Trading System</title>
     <link rel="icon" href="{{asset('favicon.ico')}}">
 
